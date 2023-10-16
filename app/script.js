f2d44376446293e3f3a5bcdb82abf3af27ff180d
Vivian Nguyen <viviannguyen@Vivians-MBP-2.lan>
2023-10-16 01:05:14 -0700

    Added dashboard display, modified icons, modified landing page width

--- app/script.js
+++ app/script.js
@@ -2,3 +2,8 @@ function toggleForm(){
     var form = document.getElementById("newProjectForm");
     form.classList.toggle("visible");
 }
+
+function toggleProject(){
+    var form = document.getElementById("oneProjectDisplay");
+    form.classList.toggle("visible");
+}
